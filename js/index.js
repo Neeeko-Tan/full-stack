d94859e1ee52482d884bea5dab003e310daeb543
Jenny Tan <Jenny.Neeeko@outlook.com>
2020-03-27 19:53:21 +0800

    foo,bar,1

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
-var result = [];
-//x % 4 == 0 ? (x % 5 == 0? 0 : 1 ) : 0 ;
-for (var i = 0; i < 100; i++){
-    if (!(i%4) && (i%5)){
-        result.push(i);
-        if (result.length === 10){
-            break;
-        }
-    }
+function foo() {
+    return 1;
 }
 
-console.log(result);
+function bar() {
+    var a = foo();
+    console.log(a);
+}
+
+bar();
