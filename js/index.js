508f5cea3da30d0b3bf005217819ca7a8bc0fdec
Jenny Tan <Jenny.Neeeko@outlook.com>
2020-03-29 22:43:30 +0800

    quiz2

--- js/index.js
+++ js/index.js
@@ -1,15 +1,48 @@
-var obj ={
-    name:'victor',
-    printName: function () {
-        console.log(this.name);
+var array = [1, 3, 4, 6, 7, 8, 10, 14, 15];
+var target = 14;
+
+function twoSum(array, target){
+    for (var i = 0; i < array.length; i++){
+        for (var j=0; j < array.length; j++){
+            if (array[i] + array[j] === target && i !== j){
+                return [i,j];
+            }
+        }
     }
-};
+    return [-1,-1]
+}
+console.log(twoSum(array, target));
 
-obj.printName();
+
+function twoSum2(array, target){
+    var left = 0;
+    var right = array.length - 1;
+    while (left < right) {
+        var sum = array[left] + array[right];
+        if (sum === target){
+            return[left, right];
+        }
+        if (sum > target){
+            right--;
+        }else{
+            left++;
+        }
+    }
+    return[-1,-1];
+}
+console.log(twoSum2(array, target));
 
 
-var o= {
-    name: 'Jenny',
-};
-
-obj.printName.bind(o)();
+function twoSum3(array, target){
+    var diffs = {};
+    for (var i = 0; i < array.length; i++){
+        var currentNumber = array[i];
+        if (diffs[currentNumber]){
+            return [diffs[currentNumber], i];
+        }
+        var diff = target - currentNumber;
+        diffs[diff] = i;
+    }
+    return [-1,-1];
+}
+console.log(twoSum3(array, target));
